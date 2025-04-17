803b6c58589a3056f5fcb2843a1e04dcda80cfaf
Madboy0700 <bobimobi07@gmail.com>
2025-04-17 13:24:45 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Madboy0700/ce")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AcelyaDuyuruu")
 
